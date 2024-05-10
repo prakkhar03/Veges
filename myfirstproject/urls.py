cc7b63061db223198cdc6e1d0332e33a798bdc2a
Prakkhar <prakkhar.n03@gmail.com>
2024-05-10 22:56:08 +0530

    specified static and media url

--- myfirstproject/urls.py
+++ myfirstproject/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from vege.views import *
-
+from django.conf.urls.static import static 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 urlpatterns = [
 
     path('',receipies, name='receipes'), 
 
     path('admin/', admin.site.urls),
 ]  
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=staticfiles_urlpatterns()
