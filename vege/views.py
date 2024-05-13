058504f8f964cfe5f54cc9ff7c29a1b9cb1c784f
Prakkhar <prakkhar.n03@gmail.com>
2024-05-13 22:15:26 +0530

    specified upload and manage

--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render , redirect
 from vege.models import *
+from django.shortcuts import get_object_or_404
 
+def frontpage(request):
+    return render(request , 'frontpage.html')
 def receipies(request):
     if request.method == 'POST':
         data = request.POST
@@ -13,8 +16,21 @@ def receipies(request):
             descp=d,
             image=i
         )
-        return redirect('/receipies/')
+        return redirect('/recipe')
     queryset=receipe.objects.all()
     context={'receipies':queryset}
-    
-    return render(request, 'veges.html',context)
+    return render(request,'veges.html',context)
+
+def upload(request):
+    queryset = receipe.objects.all()
+    context = {'recipes': queryset} 
+    return render(request, 'uploaded.html', context)
+
+def manage(request):
+    queryset = receipe.objects.all()
+    context = {'recipes': queryset} 
+    return render(request, 'manage.html', context)
+def delete(request, id):
+    recipe = get_object_or_404(receipe, id=id)
+    recipe.delete()
+    return redirect('/manage/')
