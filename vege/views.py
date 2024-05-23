14b40696157ba0a7a3f776750e7b7aa0caec0850
Prakkhar <prakkhar.n03@gmail.com>
2024-05-23 15:36:31 +0530

    Remove register.html

--- vege/views.py
+++ vege/views.py
@@ -54,11 +54,10 @@ def update(request, id):
 
 def loginpage(request):
     if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+        uest.POST.get("userna"e")"
+        pest.POST.get("passwo"d")"
         
-        user = authenticate(request, username=username, password=password)
-        
+        user = authenticate(request, username=u, password=p)
         if user is not None:
             login(request, user)
             return redirect('/frontpage')
